935686b202de8236e9fb1d688f172bc4510b89f5
kurtezrael <42833509+kurtezrael@users.noreply.github.com>
2018-09-01 00:21:09 +0800

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController} from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+ 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    todos: string[] = [];
+    todo: string;
+ 
+    constructor(public navCtrl: NavController, public alertCtrl:  AlertController) {
+    this.addValid = new FormGroup({
+        name: new FormControl('', Validators.required)
+    });
+    }
+ 
+    add() {
+        this.todos.push(this.todo);
+        this.todo = "";
+    }
+ 
+    delete(item) {
+        var index = this.todos.indexOf(item, 0);
+        if (index > -1) {
+            this.todos.splice(index, 1);
+        }
+    }
 
-  constructor(public navCtrl: NavController) {
+    showAlert() {
+      let alert = this.alertCtrl.create({
+        title: '',
+        subTitle: 'Successfully added to the list.',
+        buttons: ['OK'] });
+      alert.present();
+    }
 
-  }
+    showDelAlert() {
+      let alert = this.alertCtrl.create({
+        title: '',
+        subTitle: 'Deleted.',
+        buttons: ['OK'] });
+      alert.present();
+    }
+
+    edit(item) {
+        var index = this.todos.indexOf(item, 0);
+            if (index > -1) {
+            this.todos.splice(index, 1);
+            this.todo = item;
+
+        }
+            
+        
+    }
+    
+
 
 }
